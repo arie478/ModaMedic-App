2f19cbab83494a8b23c49f055b716691e4b891b5
arie <arie478@gmail.com>
2022-12-29 19:06:19 +0200

    Fixed app connection to actual server instead of local one Connection Fix : Enable port 19000 in firewall settings and change network to private to allow detection when running android emulator

--- environment.js
+++ environment.js
@@ -4,20 +4,20 @@ import { Platform } from "react-native";
 const backendUri = 'https://rps.ise.bgu.ac.il/njsw18';
 
 const ENV = {
-    // API_URL: {
-    //     login: 'https://rps.ise.bgu.ac.il/njsw18users/login',
-    //     signup: 'https://rps.ise.bgu.ac.il/njsw18users/patientRegister',
-    //     personalDetalis:'https://rps.ise.bgu.ac.il/njsw18auth/usersAll/patientUpdateAndroid',
-    //     metrics: 'https://rps.ise.bgu.ac.il/njsw18auth/patients/metrics',
-    //     answers: 'https://rps.ise.bgu.ac.il/njsw18auth/patients/answers'
-    // },
     API_URL: {
-        login: 'http://localhost:8181/users/login',
-        signup: 'http://localhost:8181/users/patientRegister',
-        personalDetalis:'http://localhost:8181/auth/usersAll/patientUpdateAndroid',
-        metrics: 'http://localhost:8181/auth/patients/metrics',
-        answers: 'http://localhost:8181/auth/patients/answers'
+        login: 'https://rps.ise.bgu.ac.il/njsw18users/login',
+        signup: 'https://rps.ise.bgu.ac.il/njsw18users/patientRegister',
+        personalDetalis:'https://rps.ise.bgu.ac.il/njsw18auth/usersAll/patientUpdateAndroid',
+        metrics: 'https://rps.ise.bgu.ac.il/njsw18auth/patients/metrics',
+        answers: 'https://rps.ise.bgu.ac.il/njsw18auth/patients/answers'
     },
+    // API_URL: {
+    //     login: 'http://localhost:8181/users/login',
+    //     signup: 'http://localhost:8181/users/patientRegister',
+    //     personalDetalis:'http://localhost:8181/auth/usersAll/patientUpdateAndroid',
+    //     metrics: 'http://localhost:8181/auth/patients/metrics',
+    //     answers: 'http://localhost:8181/auth/patients/answers'
+    // },
 }
 
 export default ENV
