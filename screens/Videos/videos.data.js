34f017fc2ae84dca6d19c050314a81e7b996ae4c
arie <arie478@gmail.com>
2023-01-15 13:12:24 +0200

    Final implementation of Translation major feature for hackathon + Working for APK generation and build

--- screens/Videos/videos.data.js
+++ screens/Videos/videos.data.js
@@ -1,8 +1,8 @@
 export const videos = {
     Neck: [
-        { id: 'E6ipJloNHxg', description: 'פיזיותרפיה 1 - כיפוף הצוואר והראש לפנים' },
-        { id: 'sxWDOHwEvpw', description: 'פיזיותרפיה 2 - כיפוף הצוואר והראש בשכיבה' },
-        { id: 'Dqf6M8GyIbQ', description: 'פיזיותרפיה 3 - הטיית הצוואר והראש לאחור' },
+        { id: 'E6ipJloNHxg', description: 'Physiotherapy 1 - Forward bending of the neck and head' },
+        { id: 'sxWDOHwEvpw', description: 'Physiotherapy 2 -Neck and head bending while on the back' },
+        { id: 'Dqf6M8GyIbQ', description: 'Physiotherapy 3 - Backwards bending of the neck and head' },
     ],
     Shoulders: [
         { id: 'E6ipJloNHxg', description: 'פיזיותרפיה 1 - כיפוף הצוואר והראש לפנים' },
@@ -28,3 +28,5 @@ export const videosCategory = [
     'Knee',
     'Twins',
 ];
+
+
