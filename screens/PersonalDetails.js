518c0d14bece78bd69a4ff5d3a82140dd18b37fa
abeksis93 <abeksis@post.bgu.ac.il>
2023-05-30 22:52:11 +0300

    added UpdateSurgery

--- screens/PersonalDetails.js
+++ screens/PersonalDetails.js
@@ -1,11 +1,12 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import AsyncStorageLib from "@react-native-async-storage/async-storage";
 import {CredentialsContext} from "../components/CredentialsContext";
 import { Formik } from 'formik';
 import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
-
+import axios from 'axios';
+import ENV from "../environment";
 
 import {
     StyledContainer,
@@ -40,7 +41,78 @@ const { brand, darkLight, primary } = Colors;
 
 
 const PersonalDetails = ({ navigation }) => {
+  const [message, setMessage] = useState();
+  const [messageType, setMessageType] = useState();
+  const { storedCredentials } = useContext(CredentialsContext);
+  const [SurgeryDate, setSurgeryDate] = useState();
+  var usrInfo = undefined;
+  // useEffect(() => {
+  //   const fetchSurgeryDate = async () => {
+  //     handleMessage(null);
+  //     try {
+  //       const response = await axios.get(ENV.API_URL.userDetalis + "/userInfo", {
+  //         headers: { 'x-auth-token': storedCredentials.token }
+  //       })
+  //       .then((response) => {
+  //         console.log(response.data);
+  //         setSubmitting(false);
+  //         navigation.goBack();
+  //       })
+  //       .catch((error) => {
+  //           console.log('error', error);
+  //           setSubmitting(false);
+  //           handleMessage(t("Something went wrong. Please try again later."));
+  //       });
+  //       const { usrInfo } = response.data.data;
+  //       console.log(usrInfo);
+  //       console.log(usrInfo.DateOfSurgery);
+  //       DateOfSurgery = usrInfo.DateOfSurgery;
+  //       // if (DateOfSurgery) {
+  //       //   setSurgeryDate(new Date(DateOfSurgery));
+  //       // }
+  //     } catch (error) {
+  //       console.log('error', error);
+  //       handleMessage(t("Something went wrong. Please try again later."));
+  //     }
+  //   };
+  
+  //   fetchSurgeryDate();
+  // }, []);
 
+  useEffect(() => {
+    const fetchSurgeryDate = async () => {
+      handleMessage(null);
+      try {
+        const response = await axios.get(ENV.API_URL.userDetalis + "/userInfo", {
+          headers: { 'x-auth-token': storedCredentials.token }
+        })
+        .then((response) => {
+          console.log(response.data);
+          usrInfo = response.data;
+        })
+        .catch((error) => {
+            console.log('error', error);
+            handleMessage(t("Something went wrong. Please try again later."));
+        });
+
+
+        if (usrInfo.data["DateOfSurgery"] !== null) {
+          const surDate = (new Date(usrInfo.data["DateOfSurgery"])).toLocaleDateString('en-GB').toString();
+          setSurgeryDate(surDate);
+        }
+      } catch (error) {
+        console.log('error', error);
+        handleMessage(t("Something went wrong. Please try again later."));
+      }
+    };
+  
+    fetchSurgeryDate();
+  }, []);
+
+  const handleMessage = (message, type = 'FAILED') => {
+    setMessage(message);
+    setMessageType(type);
+  };
     /*
     The useTranslation hook is a custom hook provided by the react-i18next library.
     It allows you to access the t function in your functional React components.
@@ -58,12 +130,13 @@ const PersonalDetails = ({ navigation }) => {
           <PageLogo resizeMode="cover" source={require('../assets/app_logo.png')} />
           <PageTitle welcome={true}>{t('Your Personal Information')}</PageTitle>
           <StyledFormArea>
-            <SubTitle welcomeHeader={true}>{t('Surgery Date')}: 29/11/2022</SubTitle>
+            <SubTitle welcomeHeader={true}>{t('Surgery Date')}: {SurgeryDate}</SubTitle>
             <StyledButton welcome={true} onPress={() => navigation.navigate('UpdatePersonalDetails')}>
               <ButtonText welcome={true}>{t('Update Personal Information')}</ButtonText>
               <Fontisto name="eraser" color={primary} size={15} />
             </StyledButton>
-            <StyledButton welcome={true} onPress={() => Alert.alert("This feature will be available in the next update!")}>
+            <StyledButton welcome={true} onPress={() => navigation.navigate('UpdateSurgeryDate')}>
+            {/* <StyledButton welcome={true} onPress={() => Alert.alert("This feature will be available in the next update!")}> */}
               <ButtonText welcome={true}>{t('Update Surgery Date')}</ButtonText>
               <Fontisto name="date" color={primary} size={15} />
             </StyledButton>
